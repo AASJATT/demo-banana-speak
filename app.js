9c431e692cf62cdf7d29e6b102e0fa89b8bc6735
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-09-15 07:47:50 +0000

    output

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 
 
 
@@ -8,7 +10,7 @@ var txtInput = document.querySelector("#txt-input");
 
 
 function clickHandler() {
-        console.log("clicked");
-        console.log("input", txtInput.value)
+        outputDiv.innerText = "ajajajja " + txtInput.value;
+        
     }
     btnTranslate.addEventListener("click", clickHandler) 
